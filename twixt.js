67d98f41d67c0190d61ee38d11efdfae1d994ccb
Richard Remer <reanjr@gmail.com>
2016-06-19 00:19:14 -0700

    click and EventTarget functions

--- twixt.js
+++ twixt.js
@@ -1,7 +1,9 @@
 var select = require("twixt-select");
 
 module.exports = {
+    click: select.into(require("twixt-click")),
     document: require("twixt-document"),
+    EventTarget: require("twixt-event-target"),
     handle: select.into(require("twixt-handle")),
     select: select
 };
